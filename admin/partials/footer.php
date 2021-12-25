627c88e43bf8f80853d6414b2d4311d4ef408731
masum-mushfiq <67286805+masum-mushfiq@users.noreply.github.com>
2021-12-26 01:09:23 +0600

    Successfully Done

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2021 All right reserved, Food House. Developed By -<a href="https://www.linkedin.com/in/md-masum-mushfiq-9a82ab222/" target="_blank">Masum Mushfiq</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
